a98866b2c3f8c500e5dbc1cbb46748199c71dfe0
ErickMigwi <muthigaerick@gmail.com>
2023-02-09 11:12:45 +0300

    upadate files

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let checkbox= document.getElementById("checkbox")
 let completed = document.getElementById("completedTasks")
 let uncompleted = document.getElementById("uncompleted")
 let UncompletedTasks = document.getElementById("UncompletedTasks")
-//let try = document.getElementById("try")
+let completeBtn= document.getElementById("complete-btn")
 const taskArr = []
 var taskObj =   {
   task: "",
@@ -35,60 +35,64 @@ function cloneTodo() {
   }
   return clone;
 }
-addTaskBtn.addEventListener("click", (event)=>{
+addTaskBtn.addEventListener("click", (event)=>  {
   taskObj.id = Math.floor( Math.random()*1000)
-  // checkbox.addEventListener("change", function(){
-  //   taskObj.completed = checkbox.checked
-  // })
-  //console.log(taskObj.completed);
   console.log(taskObj)
-  taskArr.push( cloneTodo(taskObj) );
   console.log(taskArr);
+  let result = taskArr
   task.value= ""
  date.value= ""
   description.value = ""
+  if(taskArr){
+    taskArr.push( cloneTodo(taskObj) )
   localStorage.setItem("MyTodos", JSON.stringify(taskArr))
-  ToDos.innerHTML+= `<li><span id="task">Task:${taskObj.task}</span> <span id= "description">Description: ${taskObj.Description}</span> <span id= "date">Completion Date: ${taskObj.date}</span></li><span><button id = "${taskObj.id}" onclick= "hold()">Completed</button></span>` 
-return taskArr
-})
-
-let currentTodos = []
-currentTodos.push(JSON.parse( localStorage.getItem("MyTodos")))
-
-//console.log(currentTodos)
-let Todos= localStorage.getItem("MyTodos")
-Todos = JSON.parse(Todos)
-Todos.forEach(element => {
-  ToDos.innerHTML+= `<li><span id="task">Task:${element.task}</span> <span id= "description">Description: ${element.Description}</span> <span id= "date">Completion Date: ${element.date}</span></li><span><button onclick= "hold()">Completed</button></span>` 
-});
-function hold(item){
-  Todos=[]
-  Todos=localStorage.getItem("MyTodos")
-  Todos = JSON.parse(Todos)
-  Todos.forEach((item, index) => {
-      Todos[index].completed = true;
-    });
-
-  console.log(Todos);
-  
-}
-saveTaskBtn.addEventListener("click", function(){
-
-  Todos.forEach(element => {
-    if(element.completed){
-      
-   completed.innerHTML+=  `<li><span id="task">Task:${element.task}</span> <span id= "description">Description: ${element.Description}</span> <span id= "date">Completion Date: ${element.date}</span></li>` 
-  }});
- 
-})
-function uncomplete(){
-  Todos.forEach(element => {
-    if(element.completed===false){
-   UncompletedTasks.innerHTML+=  `<li><span id="task">Task:${element.task}</span> <span id= "description">Description: ${element.Description}</span> <span id= "date">Completion Date: ${element.date}</span></li>` 
-    }}) 
+  ToDos.innerHTML+= `<li><span id="task">Task:${taskObj.task}</span> <span id= "description">Description: ${taskObj.Description}</span> <span id= "date">Completion Date: ${taskObj.date}</span></li><span><button class="complete-todo" data-id = "${taskObj.id}">Completed</button></span>`
   }
+})
+let StoredArray= localStorage.getItem("MyTodos")
+StoredArray=JSON.parse(StoredArray)
+for(let i =0; i<StoredArray.length; i++){
+  ToDos.innerHTML+=  `<li><span id="task">Task:${StoredArray[i].task}</span> <span id= "description">Description: ${StoredArray[i].Description}</span> <span id= "date">Completion Date: ${StoredArray[i].date}</span></li><span><button class="complete-todo" data-id="${StoredArray[i].id}">Completed</button></span>`   
+}
+let completeButn = document.querySelectorAll(".complete-todo");
+completeButn.forEach(function(button) {
+  button.addEventListener("click", function(event) {
+console.log(34567)
+console.log(StoredArray);
+      let completeId = button.getAttribute("data-id"); 
+      console.log(completeId);
+      StoredArray.forEach(function(task) {
+          if(task.id == completeId) {
+              if(task.completed) { 
+                task.completed = false;
+              } else if(! task.completed){ 
+                  task.completed = true;
+              }
+          }
+      })
+      localStorage.setItem("todos", JSON.stringify(StoredArray)); 
+  })
+})
+let StoredArrayofTrue= localStorage.getItem("todos")
+ StoredArrayofTrue=JSON.parse(StoredArrayofTrue)
+ console.log(StoredArrayofTrue);
 
-
-
-
-
+ for(let i =0; i<StoredArrayofTrue.length; i++){
+  if(StoredArrayofTrue[i].completed==true){
+   completed.innerHTML+=  `<li><span id="task">Task:${StoredArrayofTrue[i].task}</span> <span id= "description">Description: ${StoredArrayofTrue[i].Description}</span> <span id= "date">Completion Date: ${StoredArrayofTrue[i].date}</span> <button class="delete" data-id="${StoredArrayofTrue[i].id}">Delete</buttton></li>`   
+}}
+ let deletebtn= document.querySelectorAll(".delete"); 
+ deletebtn.forEach(function(button) { 
+     button.addEventListener("click", function(event) {
+         let deleteId = button.getAttribute("data-id"); 
+         StoredArrayofTrue.forEach((element) =>{
+          console.log(deleteId);
+             if(element.id == deleteId) {
+                 StoredArrayofTrue.splice(element, 1)
+             }
+         })
+         localStorage.setItem("todos", JSON.stringify(StoredArrayofTrue)); // re-save new todo in localStorage
+         location.reload();
+     })
+ })
+ 
